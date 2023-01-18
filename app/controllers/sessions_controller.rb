0a982bb81a20fca3ba0c4eb6e2a096d2893eb79c
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-19 04:39:25 +0900

    ログイン、ログアウト処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(user_id: params[:session][:user_id])
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    logout if logged_in?
+    redirect_to root_url
   end
 end
