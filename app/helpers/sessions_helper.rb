a677b31a3e090e77be55d4dae3ec58fdb4e4d00f
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-19 04:24:26 +0900

    セッション処理を追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+  def login(user)
+    session[:user_key] = user.id
+  end
+
+  def current_user
+    if session[:user_key]
+      @current_user ||= User.find_by(id: session[:user_key])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def logout
+    session.delete(:user_key)
+    @current_user = nil
+  end
 end
