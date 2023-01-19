27e915d7617f7d2333b71cfae3e21ab005224a59
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-20 08:17:15 +0900

    ログイン処理を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,26 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.find_by(user_id: params[:session][:user_id])
-    if user && user.authenticate(params[:session][:password])
-      login user
+    @user = User.new(user_id: session_params[:user_id], password: session_params[:password])
+    if @user.valid? && (@user = User.find_by(user_id: session_params[:user_id])) && @user.authenticate(session_params[:password])
+      login @user
       redirect_to root_url
     else
-      render 'new'
+      render :new,  status: :unprocessable_entity
     end
   end
 
   def destroy
     logout if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:user_id, :password)
   end
 end
