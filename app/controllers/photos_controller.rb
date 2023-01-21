d9f9d052f688e2f64dd9ecf8069d7abc8ec37d57
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-21 17:37:09 +0900

    写真アップロードページを追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,4 +2,23 @@ class PhotosController < ApplicationController
   before_action :logged_in_user
   def index
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      redirect_to photo_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :image)
+  end
 end
