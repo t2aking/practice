c64be3a3dccb4edb3bd7ced8d8035b705c7b3635
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-21 18:13:20 +0900

    マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_055145) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.integer "user_id"
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
