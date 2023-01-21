f54daac078f25e020894e09dca55336fe7f1e5e3
Tatsuaki Morioka <racerx0075440@gmail.com>
2023-01-22 07:50:14 +0900

    OAuth認可ページの表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resource :photo, only: [:new, :create]
+
+  direct :oauth do |options|
+    uri = URI.parse("http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/oauth/authorize")
+    uri.query = options.to_param unless options.empty?
+    uri.to_s
+  end
 end
